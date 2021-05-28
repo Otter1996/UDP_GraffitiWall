acc3b689a87be7bcbc16f97d9981b19940418d36
shibaruma <61943189+shibaruma@users.noreply.github.com>
2021-05-28 23:16:05 +0800

    加入專案檔案。

---
+++ UDP塗鴉牆/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace UDP塗鴉牆
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "目標IP";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "目標Port";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(325, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(514, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "接聽Port";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(568, 7);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(718, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "連線";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 216);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ UDP塗鴉牆/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.VisualBasic.PowerPacks; //匯入VB向量繪圖功能
+
+namespace UDP塗鴉牆
+{
+    public partial class Form1 : Form
+    {
+        UdpClient U;
+        Thread th;
+        ShapeContainer C;//本機畫布物件
+        ShapeContainer D; //遠端畫布物件
+        Point stp; //繪圖起點；Point物件本身就是包含x、y的點。
+        string p;//筆畫座標字串
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            C = new ShapeContainer();
+            this.Controls.Add(C);
+            D = new ShapeContainer();
+            this.Controls.Add(D);
+        }
+
+        private void Listen()
+        {
+            int Port = Convert.ToInt32(textBox3.Text);
+            U = new UdpClient(Port);
+            //建立本機資訊
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse("127.0.0.0"), Port);
+            while (true)
+            {
+                byte[] B = U.Receive(ref EP);
+                string A = Encoding.Default.GetString(B);
+                string[] Q = A.Split('/'); //切割座標點資訊(x,y)
+                Point[] R = new Point[Q.Length];//宣告座標點陣列，大小為點座標個數
+                for (int i = 0; i < Q.Length; i++)
+                {
+                    string[] K = Q[i].Split(','); //以","切割X、Y座標(x)、(y)
+                    R[i].X = int.Parse(K[0]); //定義第i點X座標
+                    R[i].Y = int.Parse(K[1]);
+                }
+                for (int i = 0; i < Q.Length -1 ; i++) //建立線段物件
+                {
+                    LineShape L = new LineShape();
+                    L.StartPoint = R[i];
+                    L.EndPoint = R[i + 1];
+                    L.Parent = D;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                th.Abort();
+                U.Close();
+            }
+            catch
+            {
+                //忽略錯誤，繼續執行。
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e) //當滑鼠按下
+        {
+            stp = e.Location; //起點  
+            p = stp.X.ToString() + "," + stp.Y.ToString();  //起點座標紀錄
+        }
+        /// <summary>
+        /// 顯示線條於畫布上，並記錄座標點
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left) //如果按下左鍵
+            {
+                LineShape L = new LineShape();
+                L.StartPoint = stp;
+                L.EndPoint = e.Location;
+                L.Parent = C; //LineShape加入畫布C
+                stp = e.Location;
+                p += "/" + stp.X.ToString() + "," + stp.Y.ToString(); //持續記錄座標
+            }
+           
+        }
+        /// <summary>
+        /// 送出繪圖action
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int Port = int.Parse(textBox2.Text);
+            UdpClient S = new UdpClient(textBox1.Text, Port);
+            byte[] B = Encoding.Default.GetBytes(p);
+            S.Send(B, B.Length);
+            S.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            th = new Thread(Listen);
+            th.Start();
+            button1.Enabled = false;
+        }
+    }
+}
